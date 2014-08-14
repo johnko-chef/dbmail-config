02ab41d8db3ede853eb8ac13e14ce1337bf79fb6
johnko <git@johnko.ca>
2014-08-14 18:53:24 -0400

    dbmail-config attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,77 +1,49 @@
-[DBMAIL] 
-dburi                = sqlite:///var/tmp/dbmail.db
-authdriver           =
-table_prefix         = dbmail_   
-encoding             = utf8
-default_msg_encoding = utf8
-sendmail              = /usr/sbin/sendmail     
-file_logging_levels       = 7
-syslog_logging_levels     = 31
-query_time_info       = 10
-query_time_notice     = 20
-query_time_warning    = 30
-query_timeout         = 300 
-effective_user        = nobody
-effective_group       = nogroup
-bindip                = 0.0.0.0		# IPv4 only - all IP's
-timeout               = 300             
-login_timeout         = 60
-resolve_ip            = no
-authlog               = no
-logfile               = /var/log/dbmail.log        
-errorlog              = /var/log/dbmail.err        
-pid_directory         = /var/run/dbmail
-tls_cafile            =
-tls_cert              =
-tls_key               =
-tls_ciphers           =
-[LMTP]
-port                  = 24                 
-[POP]
-port                  = 110
-pop_before_smtp       = no      
-[HTTP]
-port                  = 41380
-bindip                = 127.0.0.1
-admin                 = admin:secret
-[IMAP]
-port                  = 143                
-timeout               = 4000            
-imap_before_smtp      = no     
-[SIEVE]
-port                  = 2000               
-tls_port              =
-[LDAP]
-port                  = 389
-version               = 3
-hostname              = ldap
-base_dn               = ou=People,dc=mydomain,dc=com
-bind_dn               = 
-bind_pw               = 
-scope                 = SubTree
-referrals             = yes
-user_objectclass      = top,account,dbmailUser
-forw_objectclass      = top,account,dbmailForwardingAddress
-cn_string             = uid
-field_passwd          = userPassword
-field_uid             = uid
-field_nid             = uidNumber
-min_nid               = 10000
-max_nid               = 15000
-field_cid             = gidNumber
-min_cid               = 10000
-max_cid               = 15000
-field_mail            = mail
-field_quota           = mailQuota
-field_fwdtarget       = mailForwardingAddress
-[DELIVERY]
-SIEVE                 = yes               
-SUBADDRESS            = yes          
-SIEVE_VACATION        = yes      
-SIEVE_NOTIFY          = yes
-SIEVE_DEBUG           = no          
-AUTO_NOTIFY           = no
- 
-AUTO_REPLY            = no
-suppress_duplicates	= no
-quota_failure           = hard
+#
+# Cookbook Name:: dbmail-config
+# Recipe:: default
+#
+# Author:: John Ko <git@johnko.ca>
+# Copyright 2014, John Ko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set['dbmail']['enable_imapd']  = 'yes'
+set['dbmail']['enable_lmtpd']  = 'yes'
+set['dbmail']['enable_pop3d']  = 'no'
+set['dbmail']['enable_sieved'] = 'yes'
+
+set['dbmail']['dbmail']['dburi'] = 'sqlite:///var/tmp/dbmail.db'
+set['dbmail']['dbmail']['authdriver'] = '' # for DB
+set['dbmail']['dbmail']['sendmail'] = '/usr/local/sbin/sendmail'
+set['dbmail']['dbmail']['authlog']    = 'yes'
+set['dbmail']['dbmail']['bindip'] = node['ipaddress']
+set['dbmail']['http']['bindip'] = node['ipaddress']
+
+set['dbmail']['dbmail']['tls_cafile'] = ''
+set['dbmail']['dbmail']['tls_cert'] = ''
+set['dbmail']['dbmail']['tls_key'] = ''
+set['dbmail']['dbmail']['tls_ciphers'] = ''
+
+set['dbmail']['imap']['tls_port'] = '993'
+set['dbmail']['lmtp']['tls_port'] = ''
+set['dbmail']['sieve']['tls_port'] = ''
+
+set['dbmail']['ldap']['hostname'] = 'ldap'
+default['dbmail']['ldap']['base_dn'] = 'ou=People,dc=mydomain,dc=com'
+default['dbmail']['ldap']['bind_dn'] = ''
+default['dbmail']['ldap']['bind_pw'] = ''
+default['dbmail']['ldap']['field_mail']= 'mail' # a comma-separated list of attributes to match when searching for users or forwards that match a delivery address. A match on any of them is a hit.
+default['dbmail']['ldap']['field_quota]'= 'mailQuota' # field that holds the mail-quota size for a user.
+default['dbmail']['ldap']['field_fwdtarget']= 'mailForwardingAddress' # field that holds the forwarding address.
+#default['dbmail']['ldap']['query_string'] = '(mail=%s)' # override the query string used to search for users or forwards with a delivery address.
